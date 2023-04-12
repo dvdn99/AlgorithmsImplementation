9ac1e2cc5e5669a5b4cb24a0e2bf778fb5ba560c
David Novizky <dvdn.99@gmail.com>
2023-04-12 18:19:53 +0300

    Add project files.

---
+++ Algorithms/Sort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Sort
+    {
+        #region quicksort
+        public static void QuickSort(int[] arr, int low, int high)
+        {
+            if (high <= low) return;
+
+            int pivotIndex = partition(arr, low, high);
+
+            QuickSort(arr, low, pivotIndex - 1);
+            QuickSort(arr, pivotIndex, high);
+        }
+
+        private static int partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int temp_pivot_index = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    temp_pivot_index++;
+                    swap(arr, temp_pivot_index, j);
+                }
+            }
+
+            temp_pivot_index++;
+            swap(arr, temp_pivot_index, high);
+            return temp_pivot_index;
+        }
+
+        private static void swap(int[] a, int i, int j)
+        {
+            var temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        #endregion
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Algorithms;
+
+
+int[] arr = { 2, 12, 5, 1, 4, 3, 11, 9, 10, 6 };
+Action<Object> print = (e) => Console.WriteLine(e);
+
+print("Before Sort:");
+arr.ToList().ForEach(e => print(e));
+
+Sort.QuickSort(arr, 0, arr.Length - 1);
+
+print("\nAfter sort:");
+arr.ToList().ForEach(e => print(e));
+
